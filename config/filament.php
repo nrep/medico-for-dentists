2277b276038c98ede5a3528363d0bf8d6b88d85b
nrep <elvisrugero@gmail.com>
2022-11-05 08:42:13 +0200

    Published config files

--- config/filament.php
+++ config/filament.php
@@ -12,6 +12,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use lockscreen\FilamentLockscreen\Http\Middleware\Locker;
 
 return [
 
@@ -316,6 +317,7 @@ return [
     'middleware' => [
         'auth' => [
             Authenticate::class,
+            Locker::class
         ],
         'base' => [
             EncryptCookies::class,
