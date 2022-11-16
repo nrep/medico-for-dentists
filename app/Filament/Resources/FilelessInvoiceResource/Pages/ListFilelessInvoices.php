39459d5f6dc8b933c53144970bdf43263e6284b3
Elvis Peace NDAHAYO RUGERO <elvisrugero@gmail.com>
2022-11-16 20:50:51 +0200

    Added an icon to new button

--- app/Filament/Resources/FilelessInvoiceResource/Pages/ListFilelessInvoices.php
+++ app/Filament/Resources/FilelessInvoiceResource/Pages/ListFilelessInvoices.php
@@ -13,7 +13,8 @@ class ListFilelessInvoices extends ListRecords
     protected function getActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->icon('heroicon-s-plus-circle'),
         ];
     }
 }
