bdd05e9a16f4a568f11ecb91ce149e8d42bf12c6
nrep <elvisrugero@gmail.com>
2022-12-29 09:56:44 +0200

    Bug fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('sms:send')->timezone('Africa/Cairo')
-            ->dailyAt('15:12');
+            ->dailyAt('08:30');
     }
 
     /**
