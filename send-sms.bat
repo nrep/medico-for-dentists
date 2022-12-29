5c1bb5210c4fafd38f55814dfe5a26bf08c2eedf
nrep <elvisrugero@gmail.com>
2022-12-29 09:52:08 +0200

    Send SMS

--- send-sms.bat
+++ send-sms.bat
@@ -1,3 +1,3 @@
-cd c:\xampp-8.0.2\htdocs\PROVIDENCE
+cd c:\xampp8.0.2\htdocs\PROVIDENCE
 
-c:\xampp-8.0.2\php\php.exe artisan schedule:run 1>> NUL 2>&1
+c:\xampp8.0.2\php\php.exe artisan schedule:run 1>> NUL 2>&1
