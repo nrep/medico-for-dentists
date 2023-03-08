9815faea28b49d52c7cab8bc12d279a5842e8fbe
nrep <elvisrugero@gmail.com>
2023-03-08 15:11:06 +0200

    FInish charges import

--- app/Imports/ChargeImport.php
+++ app/Imports/ChargeImport.php
@@ -14,8 +14,17 @@ class ChargeImport implements ToModel
     */
     public function model(array $row)
     {
+        if (!isset($row[6])) {
+            return null;
+        }
+
         return new Charge([
-            //
+            "charge_list_charge_type_id" => 436,
+            "name" => $row[4],
+            "price" => $row[6],
+            "valid_since" => date('Y-m-d'),
+            "created_at" => date('Y-m-d H:i:s'),
+            "updated_at" => date('Y-m-d H:i:s'),
         ]);
     }
 }
