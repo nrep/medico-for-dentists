b2e587316102d9c146941a77a1dd36a97fc19f82
nrep <elvisrugero@gmail.com>
2023-03-08 14:35:10 +0200

    Add enabled scope

--- app/Models/ChargeListChargeType.php
+++ app/Models/ChargeListChargeType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class ChargeListChargeType extends Model
 {
@@ -35,5 +36,13 @@ class ChargeListChargeType extends Model
     public function charges()
     {
         return $this->hasMany(Charge::class);
+    }
+
+    /**
+     * Scope a query to only include enabled entries.
+     */
+    public function scopeEnabled(Builder $query): void
+    {
+        $query->where('enabled', 1);
     }
 }
